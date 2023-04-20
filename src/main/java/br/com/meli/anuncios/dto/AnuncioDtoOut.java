15c67e814fc583d63090e2c4135dd63bf57b2fcb
Alexia Calmon <alexia.calmon@mercadolivre.com>
2023-04-20 11:59:07 -0300

    Exercicios Aula 2

---
+++ src/main/java/br/com/meli/anuncios/dto/AnuncioDtoOut.java
@@ -0,0 +1,14 @@
+package br.com.meli.anuncios.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnuncioDtoOut {
+    private Long id;
+    private String name;
+}
